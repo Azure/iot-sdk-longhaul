49c70026580ffbe8b6ddab2c238e75e55fb08c42
Vishnu Reddy <vishnureddy@microsoft.com>
2021-03-01 13:09:57 -0600

    Implement logger, azure monitor logging and events (#62)
    * implement logger, azure monitor logging and events
    
    * small change from code review feedback

--- nodejs/device/src/index.ts
+++ nodejs/device/src/index.ts
@@ -5,33 +5,78 @@ import {
   Signal,
   ThiefPairingProperties,
   ThiefC2dMessage,
+  AzureMonitorCustomProperties,
 } from "./types";
 import { TimeoutError } from "./timeout";
+import { Logger, LoggerSeverityLevel } from "./logger";
 import { Mqtt as ProvisioningTransport } from "azure-iot-provisioning-device-mqtt";
 import { Mqtt as IotHubTransport } from "azure-iot-device-mqtt";
 import { SymmetricKeySecurityClient } from "azure-iot-security-symmetric-key";
 import { ProvisioningDeviceClient } from "azure-iot-provisioning-device";
 import { Client, Message, Twin } from "azure-iot-device";
+import { NotImplementedError } from "azure-iot-common/dist/errors";
+import * as appInsights from "applicationinsights";
+import { EventTelemetry } from "applicationinsights/out/Declarations/Contracts";
 import { createHmac } from "crypto";
 import { v4 as uuidv4 } from "uuid";
 import { promisify } from "util";
-import { NotImplementedError } from "azure-iot-common/dist/errors";
+import * as os from "os";
 
 const provisioningHost = process.env.THIEF_DEVICE_PROVISIONING_HOST;
 const idScope = process.env.THIEF_DEVICE_ID_SCOPE;
 const groupSymmetricKey = process.env.THIEF_DEVICE_GROUP_SYMMETRIC_KEY;
 const registrationId = process.env.THIEF_DEVICE_ID;
 const requestedServicePool = process.env.THIEF_REQUESTED_SERVICE_POOL;
+const appInsightsInstrumentationKey =
+  process.env.THIEF_APP_INSIGHTS_INSTRUMENTATION_KEY;
 const runId = uuidv4();
+let runReason = process.argv[2]
+  ? process.argv[2]
+  : process.env.THIEF_RUN_REASON;
 if (
   !provisioningHost ||
   !idScope ||
   !groupSymmetricKey ||
   !registrationId ||
-  !requestedServicePool
+  !requestedServicePool ||
+  !appInsightsInstrumentationKey
 ) {
   throw new Error("Required environment variable is undefined.");
 }
+
+const azureMonitorProperties: AzureMonitorCustomProperties = {
+  sdkLanguageVersion: process.versions.node,
+  sdkLanguage: "node.js",
+  sdkVersion: require("azure-iot-device/package.json").version,
+  osType: os.type(),
+  poolId: requestedServicePool,
+  transport: "mqtt",
+  deviceId: registrationId,
+  runId: runId,
+};
+
+appInsights
+  .setup(appInsightsInstrumentationKey)
+  .setAutoCollectConsole(false, false)
+  .setAutoCollectRequests(false)
+  .setAutoCollectDependencies(false)
+  .setAutoDependencyCorrelation(false);
+appInsights.defaultClient.context.tags[
+  appInsights.defaultClient.context.keys.cloudRole
+] = "device";
+appInsights.defaultClient.context.tags[
+  appInsights.defaultClient.context.keys.cloudRoleInstance
+] = runId;
+appInsights.start();
+appInsights.defaultClient.commonProperties = azureMonitorProperties;
+
+const logger = new Logger({
+  consoleEnabled: true,
+  consoleSeverityLevel: LoggerSeverityLevel.INFO,
+  appInsightsEnabled: true,
+  appInsightsSeverityLevel: LoggerSeverityLevel.WARN,
+  appInsightsClient: appInsights.defaultClient,
+});
 
 const settings: ThiefSettings = {
   thiefMaxRunDurationInSeconds: 0,
@@ -50,6 +95,7 @@ const settings: ThiefSettings = {
 
 class DeviceApp {
   private async createDeviceClientUsingDpsGroupKey() {
+    logger.info("Starting DPS registration.");
     const deviceKey = createHmac(
       "SHA256",
       Buffer.from(groupSymmetricKey, "base64")
@@ -77,17 +123,17 @@ class DeviceApp {
         throw new Error(`DPS registration failed: ${e}`);
       }
     );
-    const connectionString =
-      "HostName=" +
-      registrationResult.assignedHub +
-      ";DeviceId=" +
-      registrationResult.deviceId +
-      ";SharedAccessKey=" +
-      deviceKey;
+    logger.info(
+      `DPS registration complete. Assigned hub: ${registrationResult.assignedHub}`
+    );
+    appInsights.defaultClient.commonProperties["hub"] =
+      registrationResult.assignedHub;
+    const connectionString = `HostName=${registrationResult.assignedHub};DeviceId=${registrationResult.deviceId};SharedAccessKey=${deviceKey}`;
     const hubClient = Client.fromConnectionString(
       connectionString,
       IotHubTransport
     );
+    logger.info("Connecting to IoT Hub.");
     await hubClient.open().catch((e: Error) => {
       throw new Error(`Opening client failed: ${e}`);
     });
@@ -97,17 +143,20 @@ class DeviceApp {
   private onReceivePairingPatch(): Promise<ThiefPairingProperties> {
     return new Promise((resolve, reject) => {
       const listener = (received: any) => {
+        appInsights.defaultClient.trackEvent({
+          name: "ReceivedPairingResponse",
+        });
         if (!received.runId || !received.serviceInstanceId) {
-          return console.warn(
+          return logger.warn(
             "runId and/or serviceInstanceId is missing. Ignoring."
           );
         }
         if (received.runId !== runId) {
-          return console.warn(
+          return logger.warn(
             `runId mismatch. Ignoring. (received ${received.runId}, expected ${runId})`
           );
         }
-        console.info(
+        logger.info(
           `Service app ${received.serviceInstanceId} claimed this device instance`
         );
         this.serviceInstanceId = received.serviceInstanceId;
@@ -126,13 +175,13 @@ class DeviceApp {
       this.twin.on("properties.desired.thief.pairing", listener);
       const timer = setTimeout(
         on_timeout,
-        1000 * this.settings.pairingRequestSendIntervalInSeconds
+        1000 * settings.pairingRequestSendIntervalInSeconds
       );
     });
   }
 
   private async pairWithService() {
-    console.info("Starting pairing operation.");
+    logger.info("Starting pairing operation.");
     const pairingRequestPatch: {
       thief: { pairing: ThiefPairingProperties };
     } = {
@@ -149,9 +198,10 @@ class DeviceApp {
 
     while (
       new Date().getTime() - startTime <=
-      1000 * this.settings.pairingRequestTimeoutIntervalInSeconds
+      1000 * settings.pairingRequestTimeoutIntervalInSeconds
     ) {
-      console.info("Updating pairing reported props: %j", pairingRequestPatch);
+      appInsights.defaultClient.trackEvent({ name: "SendingPairingRequest" });
+      logger.info("Updating pairing reported props: %j", pairingRequestPatch);
       const patchReceived = this.onReceivePairingPatch();
       await promisify(this.twin.properties.reported.update)(
         pairingRequestPatch
@@ -163,8 +213,8 @@ class DeviceApp {
         patch = await patchReceived;
       } catch (e) {
         if (e instanceof TimeoutError) {
-          console.info(
-            `Pairing response timed out after waiting for ${this.settings.pairingRequestSendIntervalInSeconds} seconds. Requesting again.`
+          logger.warn(
+            `Pairing response timed out after waiting for ${settings.pairingRequestSendIntervalInSeconds} seconds. Requesting again.`
           );
           continue;
         } else {
@@ -181,18 +231,17 @@ class DeviceApp {
           },
         },
       };
-
       await promisify(this.twin.properties.reported.update)(
         pairingAcceptPatch
       ).catch((e: Error) => {
         throw new Error(`Updating reported properties failed: ${e}`);
       });
-
-      return console.info("Pairing with service complete.");
+      appInsights.defaultClient.trackEvent({ name: "PairingComplete" });
+      return logger.info("Pairing with service complete.");
     }
 
     throw new TimeoutError(
-      `Pairing failed after trying for ${this.settings.pairingRequestTimeoutIntervalInSeconds} seconds`
+      `Pairing failed after trying for ${settings.pairingRequestTimeoutIntervalInSeconds} seconds`
     );
   }
 
@@ -204,19 +253,17 @@ class DeviceApp {
       typeof obj.thief.testC2dMessageIndex !== "number"
     ) {
       this.client.reject(msg).catch(this.handleRejectC2dError);
-      return console.warn(
-        "Issue with testC2dMessageIndex property. Rejecting."
-      );
+      return logger.warn("Issue with testC2dMessageIndex property. Rejecting.");
     }
 
     this.client.complete(msg).catch(this.handleCompleteC2dError);
     if (!this.maxReceivedIndex && this.maxReceivedIndex !== 0) {
-      console.info(
+      logger.info(
         `Received initial testC2dMessageIndex: ${(this.maxReceivedIndex =
           obj.thief.testC2dMessageIndex)}`
       );
     } else if (this.maxReceivedIndex + 1 === obj.thief.testC2dMessageIndex) {
-      console.info(
+      logger.info(
         `Received next testC2dMessageIndex: ${++this.maxReceivedIndex}`
       );
     } else if (this.maxReceivedIndex < obj.thief.testC2dMessageIndex) {
@@ -224,36 +271,36 @@ class DeviceApp {
         { length: obj.thief.testC2dMessageIndex - this.maxReceivedIndex - 1 },
         (_, i) => 1 + i + this.maxReceivedIndex
       );
-      console.warn(
+      logger.warn(
         `Received testC2dMessageIndex ${obj.thief.testC2dMessageIndex}, but never received indices ${missing}`
       );
       this.maxReceivedIndex = obj.thief.testC2dMessageIndex;
       if (
         (this.c2dMissingMessageCount += missing.length) >
-        this.settings.receiveC2dAllowedMissingMessageCount
+        settings.receiveC2dAllowedMissingMessageCount
       ) {
         this.stopDeviceApp(
           new Error(
-            `The number of missing C2D messages exceeds receiveC2dAllowedMissingMessageCount of ${this.settings.receiveC2dAllowedMissingMessageCount}.`
+            `The number of missing C2D messages exceeds receiveC2dAllowedMissingMessageCount of ${settings.receiveC2dAllowedMissingMessageCount}.`
           )
         );
       }
     } else {
-      console.warn(`Received old index: ${obj.thief.testC2dMessageIndex}`);
+      logger.warn(`Received old index: ${obj.thief.testC2dMessageIndex}`);
     }
   }
 
   private handleServiceAckResponseMessage(obj: ThiefC2dMessage, msg: Message) {
     if (!Array.isArray(obj.thief.serviceAcks)) {
       this.client.reject(msg).catch(this.handleRejectC2dError);
-      return console.warn("Issue with serviceAcks property. Rejecting.");
+      return logger.warn("Issue with serviceAcks property. Rejecting.");
     }
-    console.info(`Received serviceAckIds: ${obj.thief.serviceAcks}`);
+    logger.info(`Received serviceAckIds: ${obj.thief.serviceAcks}`);
     obj.thief.serviceAcks.forEach((id: string) => {
       if (this.serviceAckWaitList.has(id)) {
         this.serviceAckWaitList.delete(id);
       } else {
-        console.warn(`Received unknown serviceAckId: ${id}`);
+        logger.warn(`Received unknown serviceAckId: ${id}`);
       }
     });
     this.client.reject(msg).catch(this.handleRejectC2dError);
@@ -263,28 +310,28 @@ class DeviceApp {
     if (e instanceof NotImplementedError) {
       return;
     }
-    console.warn(`Error when rejecting C2D message: ${e}`);
+    logger.error(`Error when rejecting C2D message: ${e}`);
     if (
       ++this.clientLibraryExceptionCount >
-      this.settings.thiefAllowedClientLibraryExceptionCount
+      settings.thiefAllowedClientLibraryExceptionCount
     ) {
       this.stopDeviceApp(
         new Error(
-          `The number of client errors exceeds thiefAllowedClientLibraryExceptionCount of ${this.settings.thiefAllowedClientLibraryExceptionCount}.`
+          `The number of client errors exceeds thiefAllowedClientLibraryExceptionCount of ${settings.thiefAllowedClientLibraryExceptionCount}.`
         )
       );
     }
   };
 
   private handleCompleteC2dError = (e: Error) => {
-    console.warn(`Error when completing C2D message: ${e}`);
+    logger.error(`Error when completing C2D message: ${e}`);
     if (
       ++this.clientLibraryExceptionCount >
-      this.settings.thiefAllowedClientLibraryExceptionCount
+      settings.thiefAllowedClientLibraryExceptionCount
     ) {
       this.stopDeviceApp(
         new Error(
-          `The number of client errors exceeds thiefAllowedClientLibraryExceptionCount of ${this.settings.thiefAllowedClientLibraryExceptionCount}.`
+          `The number of client errors exceeds thiefAllowedClientLibraryExceptionCount of ${settings.thiefAllowedClientLibraryExceptionCount}.`
         )
       );
     }
@@ -296,7 +343,7 @@ class DeviceApp {
       obj = JSON.parse(msg.getData().toString());
     } catch {
       this.client.reject(msg).catch(this.handleRejectC2dError);
-      return console.warn("Failed to parse received C2D message. Rejecting.");
+      return logger.error("Failed to parse received C2D message. Rejecting.");
     }
 
     if (
@@ -305,14 +352,14 @@ class DeviceApp {
       obj.thief.serviceInstanceId !== this.serviceInstanceId
     ) {
       this.client.reject(msg).catch(this.handleRejectC2dError);
-      console.warn("C2D received, but it's not for us: %j. Rejecting.", obj);
+      logger.warn("C2D received, but it's not for us: %j. Rejecting.", obj);
     } else if (obj.thief.cmd === "serviceAckResponse") {
       this.handleServiceAckResponseMessage(obj, msg);
     } else if (obj.thief.cmd === "testC2d") {
       this.handleTestC2dMessage(obj, msg);
     } else {
       this.client.reject(msg).catch(this.handleRejectC2dError);
-      console.warn("Unknown command received: %j. Rejecting", obj);
+      logger.error("Unknown command received: %j. Rejecting", obj);
     }
   };
 
@@ -321,13 +368,13 @@ class DeviceApp {
       thief: {
         testControl: {
           c2d: {
-            messageIntervalInSeconds: this.settings.receiveC2dIntervalInSeconds,
+            messageIntervalInSeconds: settings.receiveC2dIntervalInSeconds,
             send: true,
           },
         },
       },
     };
-    console.info("Enabling C2D message testing: %j", patch);
+    logger.info("Enabling C2D message testing: %j", patch);
     await promisify(this.twin.properties.reported.update)(patch).catch(
       (e: Error) => {
         throw new Error(`Updating reported properties failed: ${e}`);
@@ -336,17 +383,6 @@ class DeviceApp {
   }
 
   private onTelemetrySendInterval = () => {
-    if (
-      this.serviceAckWaitList.size >
-      this.settings.sendMessageAllowedFailureCount
-    ) {
-      return this.stopDeviceApp(
-        new Error(
-          `The number of service acks being waited on exceeds sendMessageAllowedFailureCount of ${this.settings.sendMessageAllowedFailureCount}.`
-        )
-      );
-    }
-
     const serviceAckId = uuidv4();
     const message = new Message(
       JSON.stringify({
@@ -359,24 +395,34 @@ class DeviceApp {
         },
       })
     );
+    logger.info(`Sending message with serviceAckId: ${serviceAckId}`);
     this.client
       .sendEvent(message)
       .catch(() => {
-        console.warn(
-          "Error sending message with serviceAckId: " + serviceAckId
+        logger.error(
+          `Error sending message with serviceAckId: ${serviceAckId}`
         );
       })
       .finally(() => {
         this.serviceAckWaitList.add(serviceAckId);
+        if (
+          this.serviceAckWaitList.size > settings.sendMessageAllowedFailureCount
+        ) {
+          this.stopDeviceApp(
+            new Error(
+              `The number of service acks being waited on exceeds sendMessageAllowedFailureCount of ${settings.sendMessageAllowedFailureCount}.`
+            )
+          );
+        }
       });
   };
 
   private async startTestOperations() {
     this.client.on("message", this.c2dMessageListener);
-    console.info("Starting telemetry sending.");
+    logger.info("Starting telemetry sending.");
     this.telemetrySendingInterval = setInterval(
       this.onTelemetrySendInterval,
-      1000 * this.settings.sendMessageOperationsPerSecond
+      1000 * settings.sendMessageOperationsPerSecond
     );
     await this.startC2dMessageSending().catch((e: Error) => {
       throw new Error(`Starting C2D message sending failed: ${e}`);
@@ -385,9 +431,11 @@ class DeviceApp {
 
   stopDeviceApp(reason?: Signal | Error) {
     let code: number;
+    let exitReasonString: string;
     if (reason instanceof Error) {
       code = 1;
-      console.error(`Stopping device app due to error: ${reason}`);
+      exitReasonString = `Stopping device app due to error: ${reason}`;
+      logger.critical(exitReasonString);
     } else {
       code = 128 + reason;
       const signal =
@@ -396,8 +444,13 @@ class DeviceApp {
           : reason === Signal.SIGINT
           ? "SIGINT"
           : "SIGKILL";
-      console.warn("Stopping device app due to signal" + signal);
+      exitReasonString = `Stopping device app due to signal ${signal}`;
+      logger.warn(exitReasonString);
     }
+    appInsights.defaultClient.trackEvent({
+      name: "EndingRun",
+      properties: { exitReason: exitReasonString },
+    });
 
     clearInterval(this.telemetrySendingInterval);
     clearTimeout(this.pairingAttemptTimer);
@@ -407,20 +460,25 @@ class DeviceApp {
     }
     if (this.client) {
       this.client.removeAllListeners();
-      console.log("Closing device client.");
+
+      logger.info("Closing device client.");
       this.client
         .close()
-        .then(() => console.log("Device client closed."))
-        .catch((e: Error) => console.error(`Error closing client: ${e}`))
+        .then(() => logger.info("Device client closed."))
+        .catch((e: Error) => logger.error(`Error closing client: ${e}`))
         .finally(() => process.exit(code));
       return;
     }
     process.exit(code);
   }
 
-  async startDeviceApp(settings: ThiefSettings) {
-    console.info("Starting device app.");
-    this.settings = settings;
+  async startDeviceApp() {
+    logger.info("Starting device app.");
+    const startingRunEvent: EventTelemetry = { name: "StartingRun" };
+    if (runReason) {
+      startingRunEvent.properties = { runReason: runReason };
+    }
+    appInsights.defaultClient.trackEvent(startingRunEvent);
     try {
       this.client = await this.createDeviceClientUsingDpsGroupKey();
       this.twin = await this.client.getTwin();
@@ -439,7 +497,6 @@ class DeviceApp {
   private c2dMissingMessageCount: number;
   private clientLibraryExceptionCount: number;
   private serviceInstanceId: string;
-  private settings: ThiefSettings;
   private client: Client;
   private twin: Twin;
 
@@ -467,5 +524,5 @@ process.on("SIGTERM", () =>
 );
 
 DeviceApp.getInstance()
-  .startDeviceApp(settings)
-  .catch(() => console.error("Device app failed."));
+  .startDeviceApp()
+  .catch(() => logger.critical("Device app failed."));
