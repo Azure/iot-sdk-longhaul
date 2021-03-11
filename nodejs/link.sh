88cac6cc4940efcfe17b36a3fe7acd70b49dc4e6
Bert Kleewein <bertk@microsoft.com>
2021-03-11 11:21:46 -0800

    move file and PR feedback

---
+++ nodejs/link.sh
@@ -0,0 +1,22 @@
+# !/bin/bash
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for
+# full license information.
+set -e 
+script_dir=$(cd "$(dirname "$0")" && pwd)
+
+NODE_SDK_ROOT=$1
+if [ ! -f ${NODE_SDK_ROOT}/tslint.json ]; then
+  echo Usage: $0 [node_root]
+  echo ex: $0 /home/repos/node
+  exit 1
+fi
+
+cd ${script_dir}/device
+npm link \
+    ${NODE_SDK_ROOT}/common/core \
+    ${NODE_SDK_ROOT}/device/core\
+    ${NODE_SDK_ROOT}/device/transport/mqtt \
+    ${NODE_SDK_ROOT}/provisioning/device \
+    ${NODE_SDK_ROOT}/provisioning/transport/mqtt \
+    ${NODE_SDK_ROOT}security/symmetric
